6cb42de14983137c6f1a3a903c51f8d722060f4a
Sonia Kolay <sonia210804@gmail.com>
2025-02-07 01:01:03 +0530

    Added Practical 1 with respective files consisting of relevant codes

---
+++ Practical_1.py/Caesar.py
@@ -0,0 +1,67 @@
+#    Encryption Function
+
+def caesar_encrypt(realText, step):
+	outText = []
+	cryptText = []
+	
+	uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+	for eachLetter in realText:
+		if eachLetter in uppercase:
+			index = uppercase.index(eachLetter)
+			crypting = (index + step) % 26
+			cryptText.append(crypting)
+			newLetter = uppercase[crypting]
+			outText.append(newLetter)
+            
+		elif eachLetter in lowercase:
+			index = lowercase.index(eachLetter)
+			crypting = (index + step) % 26
+			cryptText.append(crypting)
+			newLetter = lowercase[crypting]
+			outText.append(newLetter)
+	return outText
+
+
+#    Decryption Function
+
+def caesar_decrypt(realText, step):
+	outText = []
+	cryptText = []
+	
+	uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+	for eachLetter in realText:
+		if eachLetter in uppercase:
+			index = uppercase.index(eachLetter)
+			crypting = (index - step + 26) % 26
+			cryptText.append(crypting)
+			newLetter = uppercase[crypting]
+			outText.append(newLetter)
+            
+		elif eachLetter in lowercase:
+			index = lowercase.index(eachLetter)
+			crypting = (index - step + 26) % 26
+			cryptText.append(crypting)
+			newLetter = lowercase[crypting]
+			outText.append(newLetter)
+	return outText
+
+# USER INPUTS
+
+txt = input("Enter your plaintext: ")
+step = int(input("Enter amount of shift: "))
+
+encryptedText = caesar_encrypt(txt , step)
+decryptedText = caesar_decrypt(encryptedText , step)
+
+
+
+print("The Encrypted Text is:")
+print(encryptedText)
+print("------------------------------------")
+input("Press Enter to continue...")
+print("The Decrypted Text is:")
+print(decryptedText)
